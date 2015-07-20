3e4644a120366891fb454b8ecf07830ac30f648b
Pavel Grinchenko <psdcoder@gmail.com>
2015-07-21 00:35:36 +0300

    Decoupled to different files. Updated npm modules and changed gulp config.

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,42 @@
 var gulp = require('gulp');
+var path = require('path');
 var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
 var paths = {
-    serve: './demo/',
-    demoStyles: './demo/css/',
-    src: './src/',
-    files: {
-        demo: {
-            scss: './demo/css/*.scss',
-            html: './demo/*.html'
-        },
-        src: {
-            scss: './src/*.scss'
-        }
+    serve: 'demo',
+    demo: {
+        scss: 'demo/css/styles.scss',
+        html: 'demo/*.html'
+    },
+    src: {
+        watch: 'src/**/*.scss'
     }
 };
 
 gulp.task('default', ['serve']);
 gulp.task('build', ['scss']);
-
-
 gulp.task('serve', ['scss', 'watch'], function() {
     browserSync({ server: paths.serve });
 });
 
 gulp.task('scss', function () {
-    var taskName = process.argv.slice(2, 3)[0];
-    var isBuildTask = taskName === 'build';
-
-    return $.rubySass(paths.demoStyles, { sourcemap: true })
-        .on('error', function (err) {
-            console.error('Gulp ruby sass error', err.message);
-        })
+    return gulp.src(paths.demo.scss)
+        .pipe($.sourcemaps.init())
+        .pipe($.sass())
+        .pipe($.plumber())
         .pipe($.sourcemaps.write('./'))
-        .pipe(gulp.dest(paths.demoStyles))
-        .pipe($.if(!isBuildTask, reload({stream: true})));
-
-    // for libsas building, but when it will updates to 3.2 version
-    //gulp.src(paths.files.scss)
-    //    .pipe($.plumber())
-    //    .pipe($.sourcemaps.init())
-    //    .pipe($.sass())
-    //    .pipe($.sourcemaps.write('./'))
-    //    .pipe(gulp.dest(paths.src))
-    //    .pipe($.if(!isBuildTask, reload({stream: true})));
+        .pipe(gulp.dest(path.dirname(paths.demo.scss)))
+        .pipe(browserSync.stream({ match: '**/*.css' }));
 });
 
 gulp.task('watch', function () {
-    gulp.watch(paths.files.demo.scss, ['scss']);
-    gulp.watch(paths.files.src.scss, ['scss']);
-    gulp.watch(paths.files.demo.html).on('change', reload);
+    $.watch(paths.demo.scss, function () {
+        gulp.start('scss');
+    });
+    $.watch(paths.src.watch, function () {
+        gulp.start('scss');
+    });
+    $.watch(paths.demo.html).on('change', reload);
 });
